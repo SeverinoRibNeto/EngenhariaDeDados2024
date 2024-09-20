f0b6491e77c4026bb1d0de1adb73fc7f8b888433
Severino Ribeiro Neto <62965664+SeverinoRibNeto@users.noreply.github.com>
2024-09-19 21:42:49 -0300

    Create a class RickAndMortyAPI to consume data

--- 01-ConsumoAPI/rick_and_morty.py
+++ 01-ConsumoAPI/rick_and_morty.py
@@ -1,1 +1,29 @@
+import requests
+
+
 base_url = "https://rickandmortyapi.com/api"
+
+all_urls = requests.get(base_url)
+"""
+Retornos do get:
+    {
+        'characters': 'https://rickandmortyapi.com/api/character',
+        'locations': 'https://rickandmortyapi.com/api/location',
+        'episodes': 'https://rickandmortyapi.com/api/episode'
+    }
+"""
+
+class RickAndMortyApi:
+    def __init__(self, url = "https://rickandmortyapi.com/api/"):
+        self.url = url 
+    ## TODO
+    def getAllEpisodes(self):
+        pass
+
+    def getAllCharacters(self):
+        pass
+
+    def getAllLocation(self):
+        pass
+
+
